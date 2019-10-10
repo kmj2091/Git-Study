4c9341f612e982d5912cd4c5d2c87da93b802c4c
김민제 <kmj2091@naver.com>
2019-10-10 23:15:19 +0900

    feat : Order Text 추가

--- app/src/main/java/km/project/gitstudy/SubActivity.kt
+++ app/src/main/java/km/project/gitstudy/SubActivity.kt
@@ -6,10 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 class SubActivity : AppCompatActivity() {
 
     var text = ""
+    var textOrder = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
         text= "test"
+        textOrder ="OrderNum"
     }
 }
