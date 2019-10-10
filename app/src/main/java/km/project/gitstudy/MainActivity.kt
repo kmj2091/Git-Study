c09c80b3aac0f6309a6e64ccd819bbd3498dce7b
김민제 <kmj2091@naver.com>
2019-10-10 23:05:37 +0900

    feat :  텍스트추가

--- app/src/main/java/km/project/gitstudy/MainActivity.kt
+++ app/src/main/java/km/project/gitstudy/MainActivity.kt
@@ -5,8 +5,11 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    var text = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        text= "test"
     }
 }
