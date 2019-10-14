ed773378939ba2ade93b5de0474631db6dcdd05f
김민제 <kmj2091@naver.com>
2019-10-14 23:22:22 +0900

    feat: getText 추가

--- app/src/main/java/km/project/gitstudy/SubActivity.kt
+++ app/src/main/java/km/project/gitstudy/SubActivity.kt
@@ -11,7 +11,12 @@ class SubActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
-        text= "test"
-        textOrder ="OrderNum"
+        text = "test"
+        textOrder = "OrderNum"
+        getText()
+    }
+
+    private fun getText() {
+
     }
 }
